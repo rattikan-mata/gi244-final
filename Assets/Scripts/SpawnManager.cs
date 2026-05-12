15f771af9cf0888dc0027c031d0873d93b0ea164
pastsawutkhon <pastsawut.khon@bumail.net>
2026-05-12 09:06:27 +0700

    fix bomb, fix luckyBox ,Add asteroids

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,18 +4,17 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstaclePrefabs;
     public Transform[] spawnPoint;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating(nameof(RandomSpawn), 1f, 2f);
     }
 
+    // Update is called once per frame
     void RandomSpawn()
     {
-        if (GameManager.instance == null || !GameManager.instance.isGameActive) return;
-
         int randomPrefabIndex = Random.Range(0, obstaclePrefabs.Length);
         int randomSpawnIndex = Random.Range(0, spawnPoint.Length);
-        Instantiate(obstaclePrefabs[randomPrefabIndex], spawnPoint[randomSpawnIndex].position, Quaternion.identity);
+        Instantiate(obstaclePrefabs[randomPrefabIndex], spawnPoint[randomSpawnIndex].position, obstaclePrefabs[randomPrefabIndex].transform.rotation);
     }
 }
